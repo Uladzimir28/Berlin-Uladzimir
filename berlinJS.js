e02e0a4eafe07f3131941c7ac9bf680dae55a882
Uladzimir28 <73042616+Uladzimir28@users.noreply.github.com>
2020-11-04 20:46:03 +0300

    Add files via upload

--- berlinJS.js
+++ berlinJS.js
@@ -5,15 +5,28 @@ window.addEventListener('scroll', function (){
     }
     else{ headerLocal.style.background = 'none';}
 })
-const  ButtonPlay = document.querySelector('.button-video');
-const  video = document.querySelector('.video')
-const  contentBlock = document.querySelector('.help-content');;
+const ButtonPlay = document.querySelector('.button-circle');
+const VideoLocal= document.getElementById('Myvid')
+ButtonPlay.addEventListener('click', function (e){
 
-function Play(){
-    contentBlock.style.display = 'none';
-    video.play();
-}
-ButtonPlay.addEventListener('click',Play);
-$('.switch-btn').click(function(){
-    $(this).toggleClass('switch-on');
-});
+    if(myVideo.paused){
+        VideoLocal.play();
+    }else{
+        VideoLocal.pause();
+    }
+})
+const month = document.querySelector('.Monthly');
+const year = document.querySelector('.Yearly');
+month.addEventListener('click', function (e){
+    month.style.background = '#50E3C2';
+    month.style.borderRadius = '23px';
+    year.style.background = 'none';
+    console.log(12312);
+})
+const textYear = document.querySelectorAll('.TypeOfDollar');
+const textYearAfter = document.querySelectorAll('.TypeOfAfterDollar')
+year.addEventListener('click', function (e){
+    year.style.background = '#50E3C2';
+    year.style.borderRadius = '23px';
+    month.style.background = 'none';
+})
